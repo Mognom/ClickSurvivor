f14cad787c15b1ea8441c25db288e0359a62bd46
Mognom <alex.mognom@gmail.com>
2024-01-27 16:34:15 +0100

    Add offset to the turrets that spawn on top of the charge bar

--- Assets/Scripts/Managers/SpawnableItemsManager.cs
+++ Assets/Scripts/Managers/SpawnableItemsManager.cs
@@ -8,13 +8,13 @@ public class SpawnableItemsManager : MonoBehaviour {
     private void Start() {
         List<SpawnableItemEffects> turrets = InventoryManager.I.GetAggregatedInventoryItemEffects().SpawnableItems;
         float baseX = -1.5f;
-        float baseY = 2f;
+        float baseY = 2.5f;
         foreach (SpawnableItemEffects turret in turrets) {
 
             float spawnX = baseX + turret.Pos.Item1;
             float spawnY = baseY - turret.Pos.Item2;
             if (turret.Pos.Item2 >= 2) {
-                spawnY -= 1.5f;
+                spawnY -= 2f;
             }
             Vector3 pos = new Vector3(spawnX, spawnY, 0);
             GameObject newTurret = turret.Prefab.Spawn(pos, Quaternion.identity);
