8096679bb08c14131ec49f4d082e3a33c2b46d5b
Mognom <alex.mognom@gmail.com>
2024-01-27 13:42:51 +0100

    Add turret rotation

--- Assets/Scripts/Turrets/TurretController.cs
+++ Assets/Scripts/Turrets/TurretController.cs
@@ -1,12 +1,13 @@
-using UnityEngine;
 using MognomUtils;
-using System.Security.Cryptography;
+using UnityEngine;
 
 public class TurretController : MonoBehaviour {
     // Stats
     [SerializeField] private int baseAttacksChargeTime;
     [SerializeField] private int baseAttackDamage;
     [SerializeField] private ProyectileBehaviour proyectilePrefab;
+    [SerializeField] private float rotationTime;
+    [SerializeField] private float freezeAfterShootTime;
 
     private AudioSource turretAudioSource;
 
@@ -14,26 +15,42 @@ public class TurretController : MonoBehaviour {
     private float attackDamage;
     private float attackChargeTime;
     private float currentAttackCharge;
+    private float freezeCounter;
 
+    private EnemyBehaviour currentTarget;
+    private float rotationSpeed;
     private void OnEnable() {
         currentAttackCharge = 0;
         attackChargeTime = baseAttacksChargeTime / powerUp;
         attackDamage = baseAttackDamage * powerUp;
         turretAudioSource = GetComponent<AudioSource>();
+        freezeCounter = freezeAfterShootTime;
     }
 
     // Update is called once per frame
     private void Update() {
         currentAttackCharge += Time.deltaTime;
+        if (freezeCounter < freezeAfterShootTime) {
+            freezeCounter += Time.deltaTime;
+            return;
+        }
 
-        if (currentAttackCharge > attackChargeTime) {
+        currentTarget = WaveManager.I.GetBestTarget();
+        if (currentTarget != null) {
+            // Face towards target
+            Vector3 direction = currentTarget.transform.position - transform.position;
+            float targetAngle = Vector3.Angle(Vector3.up, direction);
+            float newZ = Mathf.SmoothDampAngle(transform.eulerAngles.z, targetAngle, ref rotationSpeed, rotationTime);
+            transform.rotation = Quaternion.Euler(0, 0, newZ);
+            //transform.up = currentTarget.transform.position - transform.position;
 
-            EnemyBehaviour target = WaveManager.I.GetBestTarget();
-            if (target != null) {
+            // Attack target
+            if (currentAttackCharge > attackChargeTime) {
                 currentAttackCharge = 0;
+                freezeCounter = 0;
                 ProyectileBehaviour newProyectile = proyectilePrefab.Spawn(transform.position, Quaternion.identity);
-                newProyectile.SetTargetAndDamage(target, attackDamage);
-                target.IncreaseIncommingDamage(attackDamage);
+                newProyectile.SetTargetAndDamage(currentTarget, attackDamage);
+                currentTarget.IncreaseIncommingDamage(attackDamage);
                 turretAudioSource.Play();
             }
         }
